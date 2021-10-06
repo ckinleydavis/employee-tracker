2a5767cfeca47155ea0354d5d63a4c66d1f16dbf
Kinley Davis <d8tascientist@gmail.com>
2021-10-05 20:13:40 -0400

    load video to assets folder, modify server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const inquirer = require("inquirer");
 const viewEmployees = require("./models/viewEmployees");
 const viewEmployeeByDepartment = require("./models/viewEmployeeByDepartment");
 const viewAllRoles = require("./models/viewAllRoles");
-const addEmployee = require("./models/addEmployee");
+// const addEmployee = require("./models/addEmployee");
 const addDepartment = require("./models/addDepartment");
 const viewAllDepartment = require("./models/viewAllDepartment");
-const addRole = require("./models/addRole");
-const updateEmployeeRole = require("./models/updateEmployeeRole");
+// const addRole = require("./models/addRole");
+// const updateEmployeeRole = require("./models/updateEmployeeRole");
 const { exit } = require("process");
 
 const connection = mysql.createConnection({
@@ -28,7 +28,8 @@ inquirer.prompt({
     name: "main",
     type: "list",
     message: "What would you like to do?",
-    choices: ["View All Employees", "View All Employees by Department", "View All Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role", "View All Departments", "Quit"]
+    // choices: ["View All Employees", "View All Employees by Department", "View All Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role", "View All Departments", "Quit"]
+    choices: ["View All Employees", "View All Employees by Department", "View All Roles", "Add Department", "View All Departments", "Quit"]
 }).then(function(answer){
     if(answer.main === "View All Employees"){
         viewEmployees(connection, init);
@@ -36,15 +37,15 @@ inquirer.prompt({
         viewEmployeeByDepartment(connection, init);
     } else if(answer.main === "View All Roles"){
         viewAllRoles(connection, init);
-    } else if(answer.main === "Add Employee"){
-        addEmployee(connection, init);
+    // } else if(answer.main === "Add Employee"){
+    //     addEmployee(connection, init);
     } else if(answer.main === "Add Department"){
         addDepartment(connection, init);
-    } else if(answer.main === "Add Role"){
-        addRole(connection, init);
-    } else if(answer.main === "Update Employee Role"){
-        updateEmployeeRole(connection, init);
-    }else if(answer.main === "View All Departments"){
+    // } else if(answer.main === "Add Role"){
+    //     addRole(connection, init);
+    // } else if(answer.main === "Update Employee Role"){
+    //     updateEmployeeRole(connection, init);
+    } else if(answer.main === "View All Departments"){
         viewAllDepartment(connection, init);
     } else if(answer.main === "Quit"){
         connection.end();
